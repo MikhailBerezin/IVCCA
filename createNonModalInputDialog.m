2fa4a7a20eb7064d9f7939410bf6d936f4a2de0c
MikhailBerezin <berezinm@wustl.edu>
2023-12-15 13:00:52 -0600

    random_sel_txt and GUI correlation

---
+++ createNonModalInputDialog.m
@@ -0,0 +1,13 @@
+function createNonModalInputDialog()
+    % Create a figure for the dialog
+    fig = uifigure('Name', 'Input', 'Position', [100, 100, 300, 150]);
+    fig.WindowStyle = 'normal'; % Make it non-modal
+
+    % Create an edit field for input
+    lbl = uilabel(fig, 'Position', [10, 120, 280, 20], 'Text', 'Enter the number of entries for plotting:');
+    ef = uieditfield(fig, 'numeric', 'Position', [10, 90, 280, 22]);
+
+    % Create a button to submit the input
+    btn = uibutton(fig, 'Position', [100, 50, 100, 22], 'Text', 'Submit');
+    btn.ButtonPushedFcn = @(btn, event) submitInput(ef, fig);
+end
